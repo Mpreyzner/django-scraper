d9d32c01b585c75e79f94d794291a9eebff9a9c5
Mpreyzner <maria.preyzner@gmail.com>
2018-12-12 20:30:21 +0100

    Configured cron, added pytest and empty test class

--- app/scraper/cron.py
+++ app/scraper/cron.py
@@ -3,7 +3,7 @@ from .post_scraper import PostScraper
 
 
 class ScraperCronJob(CronJobBase):
-    RUN_EVERY_MINS = 5
+    RUN_EVERY_MINS = 60
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'app.scraper.scraper_cron'
 
---
+++ tests/api_tests.py
@@ -0,0 +1,14 @@
+import requests
+
+# response = requests.get(url, timeout=self.timeout)
+# response.raise_for_status()
+
+
+def test_total_stats():
+    pass
+
+def test_author_stats():
+    pass
+
+def test_authors():
+    pass
