17f9ce9c7216ee1ad809c084e2b0c4b52a211251
Mpreyzner <maria.preyzner@gmail.com>
2018-12-13 23:15:18 +0100

    Style changes and small refactorings in post_scraper

--- app/scraper/post_scraper.py
+++ app/scraper/post_scraper.py
@@ -9,24 +9,26 @@ from langdetect import DetectorFactory
 
 
 class PostScraper:
-    url = 'https://teonite.com/blog'
-    next_page_class = 'older-posts'
-    timeout = 5
-    DetectorFactory.seed = 0
+    def __init__(self):
+        self.url = 'https://teonite.com/blog'
+        self.next_page_class = 'older-posts'
+        self.timeout = 5
+        DetectorFactory.seed = 0  # to make langdetect deterministic
 
     def execute(self):
         print('PostScraper: Scraping started')
         parsed_url = urlparse(self.url)
 
-        finished = False
         url = self.url
-        while not finished:
+        while True:
             print("PostScraper processing: " + url)
             self.get_posts(url)
+
             next_page_html = self.get_elements_from_page(self.get_page_soup(url), self.next_page_class)
             if len(next_page_html) == 0:
                 print('PostScraper: Scraping finished')
                 return
+
             next_page_path = next_page_html[0]['href']
             url = parsed_url.scheme + '://' + parsed_url.netloc + next_page_path
 
@@ -34,17 +36,20 @@ class PostScraper:
         title_class = 'post-title'
         parsed_url = urlparse(url)
         posts = self.get_elements_from_page(self.get_page_soup(url), title_class)
+
         for a in posts:
             title = a.text.strip()
             post_exists = Post.objects.filter(title=title).exists()
             if post_exists:
                 print('post already exists')
                 continue
+
             print('PostScraper: Scraping post: ' + title)
             post_url = a.find('a')['href']
             content, author = self.get_post_details(
                 (parsed_url.scheme + '://' + parsed_url.netloc + post_url))  # use some function for url
 
+            print('PostScraper: Found author: ' + author)
             author_exists = Author.objects.filter(name=author).exists()
             if not author_exists:
                 auth = Author(name=author, tokenized_name=unidecode.unidecode(author.lower().replace(" ", "")))
@@ -57,6 +62,7 @@ class PostScraper:
             if language not in ['pl', 'en']:
                 raise Exception(
                     'Received unexpected language: pl or en expected, got: ' + language + content)
+
             post = Post(title=title, author=auth, content=content, language=language)
             post.save()
             post_scraped.emit(post)
@@ -67,6 +73,7 @@ class PostScraper:
         author_class = 'author-content'
 
         soup = self.get_page_soup(post_url)
+
         author_html = self.get_elements_from_page(soup, author_class)
         if len(author_html) == 0:
             raise Exception('html with author not found, searched for class:' + author_class + ' on page:' + post_url)
